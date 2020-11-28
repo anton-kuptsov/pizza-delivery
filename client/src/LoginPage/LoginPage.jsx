51f87f67eaa103b372a2d9d0ca36f42a6405e653
Anton Kuptsov <steambot33@gmail.com>
2020-11-28 13:21:53 +0300

    [feat](LoginPage) add react-hook-form

--- client/src/LoginPage/LoginPage.jsx
+++ client/src/LoginPage/LoginPage.jsx
@@ -1,19 +1,38 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export const LoginPage = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = data => {};
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <legend>Login</legend>
           <label htmlFor="email">
             Email:
-            <input type="email" id="email" placeholder="email@" />
+            <input
+              ref={register}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email@"
+            />
           </label>
           <label htmlFor="password">
             Password:
-            <input type="password" id="password" placeholder="password" />
+            <input
+              ref={register}
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+            />
           </label>
+          <div>
+            <button>Login</button>
+          </div>
         </fieldset>
       </form>
     </div>
