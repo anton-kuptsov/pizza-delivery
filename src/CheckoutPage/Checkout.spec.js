4a4296013c690179814086a627dd2fe19d2b6869
Anton Kuptsov <steambot33@gmail.com>
2020-11-29 11:59:58 +0300

    [test](Checkout) add navigates test

--- src/CheckoutPage/Checkout.spec.js
+++ src/CheckoutPage/Checkout.spec.js
@@ -8,11 +8,9 @@ describe("Checkout", () => {
     const history = createMemoryHistory();
     const { container, getByText } = render(
       <Router history={history}>
-        <CheckoutPage
-         />
+        <CheckoutPage />
       </Router>
     );
-
     expect(container.getElementsByTagName("div")).toBeTruthy();
     expect(container.getElementsByClassName("container")).toBeTruthy();
     expect(getByText("Size:")).toBeInTheDocument();
@@ -26,6 +24,16 @@ describe("Checkout", () => {
     expect(getByText("Meat:")).toBeInTheDocument();
     expect(getByText("Total cost:")).toBeInTheDocument();
     expect(container).toHaveTextContent("200 RUB");
+  });
+
+  it("navigates to order page", () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <CheckoutPage />
+      </Router>
+    );
+
     fireEvent.click(getByText("Order"));
     expect(history.location.pathname).toEqual("/order");
   });
