5e2ab8263fbc4ede7bc4ddcf29da161e7d4a250c
Anton Kuptsov <steambot33@gmail.com>
2020-12-19 00:47:27 +0300

    [test] fix Configurator tests

--- client/src/Configurator.spec.js
+++ client/src/Configurator.spec.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
-import { render, fireEvent, act } from "@testing-library/react";
+import { render, fireEvent, act, waitFor } from "@testing-library/react";
 import Configurator from "./Configurator";
+import { Provider } from "react-redux";
+import { ingredientsReducer } from "state/ingredients/reducer";
+import { combineReducers, createStore } from "redux";
+import { pizzaReducer } from "state/pizza/reducer";
+
+const store = createStore(
+  combineReducers({
+    ingredients: ingredientsReducer,
+    pizza: pizzaReducer
+  })
+);
 
 describe("Configurator", () => {
   it("renders correctly", () => {
     const { container } = render(
-      <Configurator
-        _usePizzaHook={() => ({
-          setPizzaConfig: () => {}
-        })}
-      />
+      <Provider store={store}>
+        <Configurator />
+      </Provider>
     );
     expect(container).toHaveTextContent("Pizza Configurator");
   });
@@ -19,11 +28,9 @@ describe("Configurator", () => {
   describe("with default options", () => {
     it("minimal cost", () => {
       const { getByText } = render(
-        <Configurator
-          _usePizzaHook={() => ({
-            setPizzaConfig: () => {}
-          })}
-        />
+        <Provider store={store}>
+          <Configurator />
+        </Provider>
       );
       expect(getByText("Checkout 200 RUB")).toBeInTheDocument();
     });
@@ -31,15 +38,12 @@ describe("Configurator", () => {
 
   describe("with additional option checked", () => {
     it("add options", async () => {
-      const mockSetPizzaConfig = jest.fn();
       const history = createMemoryHistory();
       const { getByText } = render(
         <Router history={history}>
-          <Configurator
-            _usePizzaHook={() => ({
-              setPizzaConfig: mockSetPizzaConfig
-            })}
-          />
+          <Provider store={store}>
+            <Configurator />
+          </Provider>
         </Router>
       );
       fireEvent.click(getByText("Большая (35см)"));
@@ -47,29 +51,28 @@ describe("Configurator", () => {
       await act(async () => {
         fireEvent.click(getByText("Checkout 279 RUB"));
       });
-
-      expect(mockSetPizzaConfig).toBeCalledWith({
-        size: "big",
-        dough: "thin",
-        sauce: "tomato",
-        cheese: [],
-        veggies: [],
-        meat: ["beacon"]
+      expect(store.getState()).toEqual({
+        ingredients: [],
+        pizza: {
+          size: "big",
+          dough: "thin",
+          sauce: "tomato",
+          cheese: [],
+          veggies: [],
+          meat: ["beacon"]
+        }
       });
     });
   });
 
   describe("on pizza submit", () => {
     it("navigates to checkout", async () => {
-      const mockSetPizzaConfig = jest.fn();
       const history = createMemoryHistory();
       const { getByText } = render(
         <Router history={history}>
-          <Configurator
-            _usePizzaHook={() => ({
-              setPizzaConfig: mockSetPizzaConfig
-            })}
-          />
+          <Provider store={store}>
+            <Configurator />
+          </Provider>
         </Router>
       );
       await act(async () => {
